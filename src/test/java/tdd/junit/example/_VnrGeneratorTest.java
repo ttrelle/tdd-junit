4da8f07b216d1d1782843d69ab34caab95b77c6d
tobias <tobias@localhost>
2017-09-20 11:15:20 +0200

    examples migrated to JUnit5

--- src/test/java/tdd/junit/example/_VnrGeneratorTest.java
+++ src/test/java/tdd/junit/example/_VnrGeneratorTest.java
@@ -1,25 +1,25 @@
 package tdd.junit.example;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.LocalDate;
 import java.time.Month;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class _VnrGeneratorTest {
 	private _VnrGenerator generator; /** Unit under test. */
 	private LocalDate stichtag;
 	
 	/** Fixture */
-	@Before public void setUp() { 
+	@BeforeEach void setUp() { 
 		generator = new _VnrGenerator();
 		stichtag = LocalDate.of(2017, Month.FEBRUARY, 17);
 	}
 	
-	@Test public void vnr_erzeugung() {
+	@Test void vnr_erzeugung() {
 		// gegeben sei
 		String sachgebiet = "LN";
 		String name = "Fasel";
@@ -31,7 +31,7 @@ public class _VnrGeneratorTest {
 		assertThat(vnr, is("LN-2017-02-17-F001"));
 	}
 	
-	@Test public void vnr_erzeugung_ph() {
+	@Test void vnr_erzeugung_ph() {
 		// gegeben sei
 		String sachgebiet = "PH";
 		String name = "Rollt";
--- src/test/java/tdd/mockito/example/_VnrGenerator2Test.java
+++ src/test/java/tdd/mockito/example/_VnrGenerator2Test.java
@@ -8,33 +8,26 @@ import static org.mockito.BDDMockito.then;
 import java.time.LocalDate;
 import java.time.Month;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.Mock;
+
+import name.falgout.jeffrey.testing.junit5.MockitoExtension;
 import tdd.junit.VnrDao;
 import tdd.junit.example._VnrGenerator2;
 
+@ExtendWith(MockitoExtension.class)
 public class _VnrGenerator2Test {
 	
 	private _VnrGenerator2 generator; /** Unit under test. */
 	private LocalDate stichtag;
-	
-	@Rule public MockitoRule mockRule = MockitoJUnit.rule();	
-	
-	@Mock private VnrDao dao;
-	
-	/** Fixture */
-	@Before public void setUp() { 
+
+	@Test public void vnr_erzeugung(@Mock VnrDao dao) {
+		
+		// gegeben sei
 		generator = new _VnrGenerator2(dao);
 		stichtag = LocalDate.of(2017, Month.FEBRUARY, 17);
-	}
-	
-	@Test public void vnr_erzeugung() {
-		// gegeben sei
 		String sachgebiet = "LN";
 		String name = "Fasel";
 		given(dao.naechsteFreieLaufnummer(sachgebiet, name)).willReturn(1);
